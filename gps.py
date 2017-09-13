25f74d33e6a6cdc14009a84781a3776fcd362311
Jaime Marquez <harime.fi@gmail.com>
2017-09-13 16:12:52 -0500

    Class gps add

---
+++ gps.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import serial
+
+class GPS(object):
+	"""docstring for GPS"""
+	def __init__(self):
+		self.latitude =0.0
+		self.longitude =0.0
+		self.altitude =0.0
+		self.time
+
+		
+
+
